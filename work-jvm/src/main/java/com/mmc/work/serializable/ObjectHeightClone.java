e1967458c70381ef0711762a969a8d8892dbf395
gaowei <gao_wei0115@sina.com>
2018-03-15 17:57:08 +0800

    java api分析

---
+++ work-jvm/src/main/java/com/mmc/work/serializable/ObjectHeightClone.java
@@ -0,0 +1,12 @@
+package com.mmc.work.serializable;
+
+/**
+ * @packageName：com.mmc.work.serializable
+ * @desrciption: 对象深复制
+ *                      通过序列化实现
+ * @author: gaowei
+ * @date： 2018-03-15 17:55
+ * @history: (version) author date desc
+ */
+public class ObjectHeightClone {
+}
